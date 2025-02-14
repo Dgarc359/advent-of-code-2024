3fbb0c479fc9d30dead02dc10ad95b91f7e3c3b6
David Garcia <87497958+Dgarc359@users.noreply.github.com>
2025-02-14 07:51:55 -0500

    clean up day 19

--- 19/part-01.ts
+++ 19/part-01.ts
@@ -1,4 +1,3 @@
-import { Queue } from "@/util/queue";
 import * as fs from "node:fs";
 
 const input = fs.readFileSync("./input.txt").toString().split("\n");
@@ -12,16 +11,6 @@ const availablePatterns = input[0].split(",").map((el) => {
   return pattern;
 });
 const requestedPatterns = input.slice(2);
-
-const largestAvailablePattern = availablePatterns.reduce((p, c) => {
-  if (p.length >= c.length) {
-    return p;
-  } else {
-    return c;
-  }
-}).length;
-
-// console.log("got avail patterns and requested patterns");
 
 // this functions returns true if the given string is a member of the language
 // false otherwise
@@ -59,25 +48,33 @@ function CYKAlgorithm(
       startOfSpan++
     ) {
       for (
-        let partitionOfSpan = 0;
-        partitionOfSpan <= length - 1;
-        partitionOfSpan++
+        let partitionOfSpanIdx = 0;
+        partitionOfSpanIdx <= length - 1;
+        partitionOfSpanIdx++
       ) {
         // partition the substring into two parts. We'll go through all the possible permutations
         // this way to see if it is 'part of the language'
         const leftSubstr = characters
           // need to remember that slice END is EXCLUSIVE
-          .slice(startOfSpan, startOfSpan+ (length - partitionOfSpan))
+          // start + (length - partitionIdx) gives us the index of where the string is broken, accounting for the shifting window
+          .slice(startOfSpan, startOfSpan + (length - partitionOfSpanIdx))
           .join("");
         const rightSubstr = characters
-          .slice(startOfSpan + (length - partitionOfSpan), startOfSpan + (partitionOfSpan + (length - partitionOfSpan)))
+          // annoyingly confusing calculations
+          // index of the partition in string is the same as above
+          // finding the index of the end of the string includes account for the shifting window. Then
+          // partitionIdx + (length - partitionIdx) gives us the length of the right substring
+          .slice(startOfSpan + (length - partitionOfSpanIdx), startOfSpan + (partitionOfSpanIdx + (length - partitionOfSpanIdx)))
           .join("");
 
+        // our language has more than just single character substrings, it might contain
+        // our left / right substring. This is our final check before we see if
+        // both our strings can be created at the same time
         if(language[leftSubstr]) {
           CYKArr[leftSubstr.length][startOfSpan].add(leftSubstr)
         }
         if(language[rightSubstr]) {
-          CYKArr[rightSubstr.length][startOfSpan + (length - partitionOfSpan)].add(rightSubstr)
+          CYKArr[rightSubstr.length][startOfSpan + (length - partitionOfSpanIdx)].add(rightSubstr)
         }
         // console.log(""); // breakpoint
 
@@ -88,7 +85,7 @@ function CYKAlgorithm(
         // of the current string, then the current string is producible
         if (
           CYKArr[leftSubstr.length][startOfSpan].has(leftSubstr) &&
-          (rightSubstr === "" || CYKArr[rightSubstr.length][startOfSpan + (length - partitionOfSpan)].has(
+          (rightSubstr === "" || CYKArr[rightSubstr.length][startOfSpan + (length - partitionOfSpanIdx)].has(
             rightSubstr
           ))
         ) {
@@ -130,51 +127,6 @@ function findPossiblePatterns(
       console.error('issue with request', request)
       console.log('some error occured', e)
     }
-
-    
-
-    // construct a map of the requested pattern's characters
-    // so we can persist which characters have been mathced with an available
-    // pattern
-    // const requestMap: boolean[] = Array(request.split("").length).fill(false);
-
-    // // use a sliding window, make the window smaller every iteration
-    // // the window grabs a slice of the string and tries to see if it can be
-    // // matched out of the available patterns
-    // request.split("").forEach((char, charIdx, arr) => {
-    //   for (let i = 0; i < largestAvailablePattern; i++) {
-    //     const window = arr.slice(charIdx, 1 + charIdx + i);
-
-    //     // the slice we took isn't as long as we expected the window to be
-    //     if (window.length <= i) {
-    //       return;
-    //     }
-
-    //     // check if the slice we took contains already solved chracters
-    //     // if it does then we need to just go to the next slice
-    //     for (let _i = charIdx; _i < charIdx + i; _i++) {
-    //       if (requestMap[_i] === true) {
-    //         return;
-    //       }
-    //     }
-
-    //     const windowStr = window.join("");
-
-    //     if (patterns[windowStr] !== undefined) {
-    //       for (let _i = charIdx; _i <= charIdx + i; _i++) {
-    //         requestMap[_i] = true;
-    //       }
-    //       break;
-    //     }
-    //   }
-    // });
-
-    // // if every character in the requested pattern has been matched with an available pattern
-    // if (requestMap.every((k) => k === true)) {
-    //   possiblePatterns++;
-    // } else {
-    //   console.log("impossible!", request);
-    // }
   }
 
   return possiblePatterns;
